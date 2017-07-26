aee7b352f4a89663a9c7cf6ab8c9ef8f008d0c27
Hardik Patel <hat@odoo.com>
2017-07-26 12:02:49 +0530

    [UPDATE] updated test case for galaxy academy.

--- galaxyacademy/tests/test_course.py
+++ galaxyacademy/tests/test_course.py
@@ -9,24 +9,10 @@ class TestSample(TransactionCase):
     	recordSession = self.env['galaxyacademy.session'].create({'name':'Session_TestSample'})
     	recordExam = self.env['galaxyacademy.exam'].create({'name':'Exam_TestSample',
     		'course_id':recordCourse.id})
+        recordExam.write({'description':'s just sa'})
+        recordSession.write({'attendee_ids':'Agrolait','seats':'10','attendees_count':'1'})
+        # print recordSession
+        recordSession.action_confirm()
+        recordCourse.write({'session_ids':recordSession})
+        # print recordCourse.session_ids
 
-    	# updateExam = self.env.ref('galaxyacademy.exam')
-    	# updateExam.write({'description':'This is just sample, not to overwrite it.'})
-    	self.env['galaxyacademy.exam'].write({'description':'s just sa'})
-        # record.some_action()
-
-        # self.assertEqual(
-        #     record.field,
-        #     expected_field_value)
-
-    	# team = self.env['crm.team'].sudo(self.crm_salemanager.id).create({'name': "Phone Marketing"})
-     #    lead = self.env.ref('crm.crm_case_1')
-     #    lead.sudo(self.crm_salemanager.id).write({'team_id': team.id})
-        # self.assertEqual(1, 0)
-
-    # def test_session(self):
-    # 	record = self.env['galaxyacademy.session'].create({'name':'Session_TestSample'})
-
-    # def test_exam(self):
-    # 	record = self.env['galaxyacademy.exam'].create({'name':'Exam_TestSample',
-    # 		'course_id':'Course_TestSample'})
